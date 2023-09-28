d885a35562b956338603e888fa47f8618082b8c6
deadlightreal <139078351+deadlightreal@users.noreply.github.com>
2023-09-28 19:33:28 +0200

    Delete LoginANDRegister directory

--- LoginANDRegister/dbConnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $sname = "localhost";
-    $unmae = "root";
-    $password = "";
-    
-    $db_name = "loginandregister";
-    
-    $sql = mysqli_connect($sname, $unmae, $password, $db_name);
-?>
