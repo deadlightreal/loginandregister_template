e2f608a62af50cf95cc15dd383bcba73626e8dc4
deadlightreal <139078351+deadlightreal@users.noreply.github.com>
2023-09-29 15:28:26 +0200

    Update Register.php

--- Register.php
+++ Register.php
@@ -5,13 +5,18 @@ if (isset($_SESSION["username"]) && $_SESSION["username"] != null) {
     header("Location: home.php");
 }
 else {
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    
-    $sqlCode = "INSERT INTO `users` (`username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
-    $mysql = mysqli_query($sql, $sqlCode);
-    header("Location: LoginScreen.php");
-    $_SESSION["username"] = $username;
+    if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["password"])) {
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        
+        $sqlCode = "INSERT INTO `users` (`username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
+        $mysql = mysqli_query($sql, $sqlCode);
+        header("Location: LoginScreen.php");
+        $_SESSION["username"] = $username;
+    }
+    else {  
+        echo "Enter username, email and password!!!";
+    }
 }
 ?>
