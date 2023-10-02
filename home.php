4fdd49c35a9a31455952f809d56c51639205382f
deadlightreal <139078351+deadlightreal@users.noreply.github.com>
2023-10-02 20:51:20 +0200

    Update home.php

--- home.php
+++ home.php
@@ -13,10 +13,30 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form action="logout.php">
         <input type="submit" value="logout">
+    </form>
+    <table class="posts">
+        <tr>
+        <?php
+        include "dbConnection.php";
+        $data = "SELECT * FROM posts";
+        $result = $sql->query($data);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr class='post'><td class='owner'>" . $row["owner"] . "</td></tr>";
+                echo "<tr class='post'><td class='posttext' colspan='2'>" . $row["text"] . "</td></tr>";
+            }
+        }
+        ?>
+        </tr>
+    </table>
+    <form action="newPost.php" method="POST">
+        <input type="submit" value="New Post">
     </form>
 </body>
 </html>
